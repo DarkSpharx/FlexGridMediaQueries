11ad102c400ea7dc5fe95a793362bf64bf29f394
fred_la_bete@hotmail.com <fred_la_bete@hotmail.com>
2025-02-25 12:17:11 +0100

    push du midi 25/02/25

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Sélectionner la barre de navigation et la section#Accueil
-const header = document.querySelector('.header'); // Sélectionne le nav avec la classe .navbar
-const main = document.getElementById('main'); // Sélectionne la section#Accueil
+const header = document.querySelector('#header'); // Sélectionne le nav avec la classe .navbar
+const main = document.getElementById('#main'); // Sélectionne la section#Accueil
 
 // Fonction pour ajuster le padding de la section#Accueil
 function adjustSectionPadding() {
   // Obtenir la hauteur de la barre de navigation
-  const header_height = navbar.offsetHeight;
+  const header_height = header.offsetHeight;
 
   // Appliquer un padding-top à la section#Accueil pour éviter que le contenu ne soit caché
-  main.style.paddingTop = `${header_height}px`;
+  main.style.paddingTop = `${header_height}rem`;
 }
 
 // Appeler la fonction pour ajuster le padding lors du chargement de la page
